b19b1bdee95b45680c5d67e53c5794225ae9a2a9
Carlos Pinheiro <carlosadap@gmail.com>
2021-03-05 14:10:56 +0100

    Add bubble_sort

---
+++ bubble_sort/bubble_sort.rb
@@ -0,0 +1,19 @@
+def bubble_sort(array)
+  is_sorted = false
+
+  until is_sorted
+    is_sorted = true
+    
+    (0...array.length-1).each do |idx| 
+      if array[idx] > array[idx+1]
+        array[idx], array[idx + 1] = array[idx + 1], array[idx]
+        is_sorted = false
+      end
+    end
+  end
+
+  array
+end
+
+p bubble_sort([4,3,78,2,0,2])
+# => [0,2,2,3,4,78]
