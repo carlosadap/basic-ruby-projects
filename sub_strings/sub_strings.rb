0200cdcb1d3e5aec2b8961e3a6af6fc6e1de6303
Carlos Pinheiro <carlosadap@gmail.com>
2021-03-05 13:05:54 +0100

    Solve first part using each and include

--- sub_strings/sub_strings.rb
+++ sub_strings/sub_strings.rb
@@ -1,3 +1,20 @@
-def sub_strings(string, array)
+def substrings(string, array)
+  words = string.split(" ")
+  substrings_hash = Hash.new(0)
 
+  array.each do |word|
+    if string.include?(word)
+      substrings_hash[word] += 1
+    end
+  end
+
+  substrings_hash
 end
+
+  dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
+  
+  puts substrings("below", dictionary)
+  # => { "below" => 1, "low" => 1 }
+
+  puts substrings("Howdy partner, sit down! How's it going?", dictionary)
+  # => { "down" => 1, "go" => 1, "going" => 1, "how" => 2, "howdy" => 1, "it" => 2, "i" => 3, "own" => 1, "part" => 1, "partner" => 1, "sit" => 1 }
