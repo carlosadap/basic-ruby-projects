8e832980af6161d794b781b5ba6daffb2676c772
Carlos Pinheiro <carlosadap@gmail.com>
2021-03-08 09:55:32 +0100

    Add subclass exercises

--- classes_and_objects/solution.rb
+++ classes_and_objects/solution.rb
@@ -13,19 +13,84 @@ end
 my_object = MyClass.new
 
 # 3 && 4
-class MyCar
+# class MyCar
+#   attr_accessor :color, :current_speed
+#   attr_reader :year, :model
+
+#   def self.gas_mileage(km, liters)
+#     puts "#{km / liters} kilometers per liter of gas"
+#   end
+
+#   def initialize(year, color, model)
+#     @year = year
+#     @color = color
+#     @model = model
+#     @current_speed = 0
+#   end
+
+#   def speed_up(num)
+#     self.current_speed += num
+#   end
+
+#   def brake(num)
+#     self.current_speed -= num
+#   end
+
+#   def shut_off
+#     self.current_speed = 0
+#   end
+
+#   def spray_paint(color)
+#     self.color = color
+#   end
+
+#   def to_s
+#     "This car is a #{color}, #{year}, #{model}"
+#   end
+# end
+
+# 5
+# change attr_reader to attr_accessor
+
+class Animal
+  def initialize
+  end
+end
+
+class Bear < Animal
+  def initialize(color)
+    super
+    @color = color
+  end
+end
+
+# 6
+module Towable
+  def can_tow?
+    @weight < 4000 ? true : false
+  end
+end
+
+class Vehicle
   attr_accessor :color, :current_speed
   attr_reader :year, :model
 
   def self.gas_mileage(km, liters)
     puts "#{km / liters} kilometers per liter of gas"
   end
+  
+  def self.number_of_vehicules
+    puts @@number_of_vehicules
+  end
+
+  @@number_of_vehicules = 0
 
   def initialize(year, color, model)
     @year = year
     @color = color
     @model = model
     @current_speed = 0
+    @@number_of_vehicules += 1
   end
 
   def speed_up(num)
@@ -49,18 +114,17 @@ class MyCar
   end
 end
 
-# 5
-# change attr_reader to attr_accessor
-
-class Animal
-  def initialize
-  end
+class MyCar < Vehicle
+  @@number_of_doors = 4
 end
 
-class Bear < Animal
-  def initialize(color)
-    super
-    @color = color
+class MyTruck < Vehicle
+  @@number_of_doors = 2
+  include Towable
+  
+  def initialize(year, color, model, weight)
+    super(year, color, model)
+    @weight = weight
   end
 end
 
