8fe11961afe462e270529710d249cf34075eab1d
Carlos Pinheiro <carlosadap@gmail.com>
2021-03-08 13:14:34 +0100

    Add board to game and check player symbol

--- tic_tac_toe/lib/game.rb
+++ tic_tac_toe/lib/game.rb
@@ -1,5 +1,25 @@
+require_relative 'player.rb'
+require_relative 'board.rb'
+
 class Game
-  def initialize(n_players = 2)
-    @players = []    
+  attr_reader :players, :symbols, :board
+
+  def initialize(length, n_players = 2)
+    @players = []
+    @board = Board.new(length)
+  end
+
+  def create_player(name, symbol)
+    if is_valid_symbol?(symbol.to_sym)
+      new_player = Player.new(name, symbol.to_sym)
+      players << new_player
+      puts "The player #{name} with the symbol #{symbol} was created"
+    else
+      puts "The player #{name} with the symbol #{symbol} was not created because the symbol #{symbol} already exists"
+    end
+  end
+
+  def is_valid_symbol?(check_symbol)
+    players.none? { |player| player.symbol == check_symbol }
   end
 end
