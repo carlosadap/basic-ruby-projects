05e2f5c666387a1e3b6f405a5659424a67eb5473
Carlos Pinheiro <carlosadap@gmail.com>
2021-03-09 14:09:03 +0100

    Add ask_guess

--- mastermind/game.rb
+++ mastermind/game.rb
@@ -5,9 +5,32 @@ class Game
     @turns_code = []
     @code_size = code_size
     @max_turns = max_turns
+    @colors = ['black', 'blue', 'white', 'red']
+  end
+
+  def guess_code(array_pegs)
+    core
+  end
+
+  def valid_guess?(array_colors)
+    color_match = array_colors.all? { |color| @colors.include?(color) }
+    qtt_match = array_colors.length == @code_size
+    return color_match && qtt_match
+  end
+
+  def ask_guess
+    puts "What is your guess?"
+    puts "Valid colors are: #{@colors}"
+    puts "Enter four valid colors, separated by comma"
+    guess = gets.chomp.split(",")
+    until valid_guess?(guess)
+      guess = ask_guess
+    end
+    guess
   end
 
   def run
     @secret_code = Code.new(@code_size)
+    guess = ask_guess
   end
 end
