c0c0d3487f9b060e1572816124e8593292a5b0b7
Carlos Pinheiro <carlosadap@gmail.com>
2021-03-11 13:21:33 +0100

    Add ending conditions

--- mastermind/game.rb
+++ mastermind/game.rb
@@ -3,7 +3,7 @@ require_relative 'code'
 class Game
   attr_reader :secret_code, :guess_code
 
-  def initialize(code_size = 4, max_turns = 12)
+  def initialize(code_size = 4, max_turns = 2)
     @turns_code = []
     @code_size = code_size
     @max_turns = max_turns
@@ -40,24 +40,34 @@ class Game
 
   def give_feedback(code)
     check_pegs = code.check_pegs(@secret_code)
-    # black_pins = code.count_black_pins(@secret_code)
-    # white_pins = code.count_white_pins(@secret_code)
     black_pins, white_pins = check_pegs
     puts 'Your score so far is:'
     puts "Black pins: #{black_pins}"
     puts "White pins: #{white_pins}"
+    display_guesses
+  end
+
+  def display_guesses
+
   end
 
   def play_turn
     guess = create_guess_code(ask_guess)
-    @game_on = false if guess.same_code?(@secret_code)
+    @turns_code << guess
+    @game_on = false if guess.same_code?(@secret_code) || out_of_turns
     give_feedback(guess)
     @secret_code.pegs.each { |peg| puts peg.color }
+  end
+
+  def out_of_turns
+    @turns_code.length >= @max_turns
   end
 
   def run
     create_secret_code
     @game_on = true
     play_turn while @game_on
+    puts "You just made the max number of attempts #{@max_turns} and didn't find the secret code" if out_of_turns
+    puts "You won!"
   end
 end
